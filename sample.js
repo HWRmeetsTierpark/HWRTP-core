2a3dc42dd928b7e19dd6adfba711e51b48a0c675
Miklas Boskamp <mboskamp@t-online.de>
2017-12-07 16:56:46 +0100

    Remove unnecessary files

--- sample.js
+++
@@ -1,9 +0,0 @@
-var sampleRequest = {
-    user: "12345678901",            //11-digit EAN code
-    request_type: {
-        mode: "authentication",     //lookup(search for user in db), authentication(biometric)
-        auth_type: "finger",        //finger, face
-        data: ""                    //image(BASE64)
-    },
-    data: ""                        //BASE64 encoded image
-}
